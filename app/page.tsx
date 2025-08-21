dd40806cc6e6a19413c80ad7566949b70d4b83a5
KevinW <s22310304@student.unklab.ac.id>
2025-08-21 12:41:30 +0800

    feat: add next/font library for custom font  and add style folder for next/font library integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 // ========= UNTUK HALAMAN HOME UBAH DISINI ===========
-
+import { Button } from "@/components/ui/button";
+import { quicksand, robotoMono, poppins } from "../style/fonts";
 
 export default function Home() {
-
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        <h2 className="text-2xl font-bold text-foreground">Hello, World</h2>
+      <h2 className="text-2xl font-bold text-foreground">Hello, World</h2>
+      <Button>Click me!</Button>
+      <p className={poppins.className}> Ini font poppins</p>
+      <p className={robotoMono.className}> Ini font robotoMono</p>
+      <p className={quicksand.className}> Ini font quicksand</p>
+      <p>ini font sans</p>
     </div>
   );
 }
