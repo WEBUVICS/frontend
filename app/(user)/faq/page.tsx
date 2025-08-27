3384144ace06a9181a8c3f204efe4b80ba208f92
SodiumZar <148340719+SodiumZar@users.noreply.github.com>
2025-08-27 19:42:44 +0800

    feat: Mengatur layout & membuat fitur tambah, delete, dan edit pengumuman
    - Mengatur layout title Announcement, tombol tambah, dan announcement card
    - Membuat tombol menambah announcement card
    - Membuat fitur tombol mengedit dan delete disetiap announcement car

--- app/(user)/faq/page.tsx
+++ app/(user)/faq/page.tsx
@@ -1,11 +1,11 @@
 // ========= UNTUK HALAMAN HOME UBAH DISINI ===========
 
 
-export default function Home() {
+export default function FAQ() {
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        <h2 className="text-2xl font-bold text-foreground">Hello, World</h2>
+        <h2 className="text-2xl font-bold text-foreground">FAQ</h2>
     </div>
   );
 }
