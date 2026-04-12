ebda473ee6fd5afb500ace2714c3d9f3d199f0b7
DaxnGo <matthewpangemanan15@gmail.com>
2026-04-13 02:04:47 +0800

    fix: upgrade next.js to patch CVE-2025-66478

--- next.config.ts
+++ next.config.ts
@@ -8,23 +8,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-     remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "res.cloudinary.com",
-    },
-    {
-      protocol: "https",
-      hostname: "picsum.photos",
-    },
-  ], 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
   },
-   eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-
 };
 
 module.exports = nextConfig;
-
