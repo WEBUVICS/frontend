cf98d6e0c229461fdfb040f04891998b330900b0
DaxnGo <matthewpangemanan15@gmail.com>
2026-04-13 01:24:13 +0800

    chore: update tahun dari 2025 ke 2026

--- app/(user)/media/dataAnnounce.ts
+++ app/(user)/media/dataAnnounce.ts
@@ -1,22 +1,13 @@
 export const DataAnnouncement = [
-    {
-        id: 'A001',
-        mainTitle: "UVICS Goes To Beach 2025",
-        title: "UVICS Goes To Beach – Sunday, 30 November 2025",
-        description: "UVICS akan mengadakan kegiatan refreshing bersama di pantai! Kegiatan ini bertujuan untuk mempererat kebersamaan dan bersama-sama menikmati waktu santai di akhir pekan. Di kegiatan ini sudah termasuk makan siang, transportation, fun games, dan beach entry. Yuk ikut dan nikmati momen seru bersama teman-teman UVICS!",
-        date: "Minggu, 30 November 2025",
-        time: "9.00 - Selesai",
-        location: "Indo Beach Lalumpe",
-        image: "/event/announcement1.webp"
-    },
-    {
-        id: 'A002',
-        mainTitle: "UVICS Goes To Beach 2025",
-        title: "UVICS Goes To Beach – Sunday, 30 November 2025",
-        description: "UVICS akan mengadakan kegiatan refreshing bersama di pantai! Kegiatan ini bertujuan untuk mempererat kebersamaan dan bersama-sama menikmati waktu santai di akhir pekan. Di kegiatan ini sudah termasuk makan siang, transportation, fun games, dan beach entry. Yuk ikut dan nikmati momen seru bersama teman-teman UVICS!",
-        date: "Minggu, 30 November 2025",
-        time: "9.00 - Selesai",
-        location: "Indo Beach Lalumpe",
-        image: "/event/announcement1.webp"
-    }
-]
+  {
+    id: "A001",
+    mainTitle: "UVICS Goes To Beach 2026",
+    title: "UVICS Goes To Beach – Sunday, 30 November 2025",
+    description:
+      "UVICS akan mengadakan kegiatan refreshing bersama di pantai! Kegiatan ini bertujuan untuk mempererat kebersamaan dan bersama-sama menikmati waktu santai di akhir pekan. Di kegiatan ini sudah termasuk makan siang, transportation, fun games, dan beach entry. Yuk ikut dan nikmati momen seru bersama teman-teman UVICS!",
+    date: "Minggu, 30 November 2025",
+    time: "9.00 - Selesai",
+    location: "Indo Beach Lalumpe",
+    image: "/event/announcement1.webp",
+  },
+];
