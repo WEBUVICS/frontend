7ea094a14b684f7a608e016f7b1a31b1eb072f7f
geraldrafael <12410054@student.unklab.ac.id>
2026-04-14 18:13:49 +0700

    Update favicon icon to use .ico format

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -29,8 +29,8 @@ export const metadata: Metadata = {
   title: "UVICS WEBSITE",
   description: "Silahkan kunjungi website uvics.",
   icons: {
-    icon: {url: '/favicon.jpg', type: 'image/jpg', }
-  }
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
