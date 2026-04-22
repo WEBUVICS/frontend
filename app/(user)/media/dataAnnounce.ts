4f531c7f3a024170fb9de4a4a0ee3c12689e76fa
Jonathan Lapian <s22310315@student.unklab.ac.id>
2026-04-22 19:46:06 +0800

    update(announcement): Updating announcement into the newest one

--- app/(user)/media/dataAnnounce.ts
+++ app/(user)/media/dataAnnounce.ts
@@ -1,13 +1,13 @@
 export const DataAnnouncement = [
   {
     id: "A001",
-    mainTitle: "UVICS Goes To Beach 2026",
-    title: "UVICS Goes To Beach – Sunday, 30 November 2025",
+    mainTitle: "UVICS Goes To Waterpark 2026",
+    title: "UVICS Goes To Waterpark – Sunday, 30 November 2025",
     description:
-      "UVICS akan mengadakan kegiatan refreshing bersama di pantai! Kegiatan ini bertujuan untuk mempererat kebersamaan dan bersama-sama menikmati waktu santai di akhir pekan. Di kegiatan ini sudah termasuk makan siang, transportation, fun games, dan beach entry. Yuk ikut dan nikmati momen seru bersama teman-teman UVICS!",
-    date: "Minggu, 30 November 2025",
+      "UVICS akan mengadakan kegiatan refreshing bersama di Waterpark! Kegiatan ini bertujuan untuk mempererat kebersamaan dan bersama-sama menikmati waktu santai di akhir pekan. Di kegiatan ini sudah termasuk makan siang, transportation, fun games, dan Waterpark entry. Yuk ikut dan nikmati momen seru bersama teman-teman UVICS!",
+    date: "Minggu, 10 Mei 2026",
     time: "9.00 - Selesai",
-    location: "Indo Beach Lalumpe",
-    image: "/event/announcement1.webp",
+    location: "Lomban Waterpark",
+    image: "/event/announcement.webp",
   },
 ];
